19d0379a9e33c786f320d218bbe291f6f3064e54
phbai <48466846@qq.com>
2017-10-04 23:27:52 +0800

    add ex5.hs

--- ch4/ex3.hs
+++ ch4/ex3.hs
@@ -1,5 +1,8 @@
 import Data.Char (digitToInt)
 
+isMinus c = c == '-'
+
 asInt_fold :: String -> Int
-asInt_fold xs = foldr step 0 xs
-    where step x xs = digitToInt x + xs
+asInt_fold xs = foldl step 0 xs
+    where step acc x | isMinus x = -acc
+                     | otherwise = digitToInt x + acc * 10
